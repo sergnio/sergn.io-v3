4169a87f27c8f398ff12cec64497fa32989c8722
sergnio <sergnioa@gmail.com>
2025-05-25 17:11:51 +0200

    add query keys

--- src/utils/users.tsx
+++ src/utils/users.tsx
@@ -1,34 +1,35 @@
-import { queryOptions } from '@tanstack/react-query'
-import axios from 'redaxios'
+import { queryOptions } from "@tanstack/react-query";
+import axios from "redaxios";
+import { GET_SINGLE_USER_KEY, GET_USERS_KEY } from "~/utils/query-keys";
 
 export type User = {
-  id: number
-  name: string
-  email: string
-}
+  id: number;
+  name: string;
+  email: string;
+};
 
-export const DEPLOY_URL = 'http://localhost:3000'
+export const DEPLOY_URL = "http://localhost:3000";
 
 export const usersQueryOptions = () =>
   queryOptions({
-    queryKey: ['users'],
+    queryKey: GET_USERS_KEY,
     queryFn: () =>
       axios
-        .get<Array<User>>(DEPLOY_URL + '/api/users')
+        .get<Array<User>>(DEPLOY_URL + "/api/users")
         .then((r) => r.data)
         .catch(() => {
-          throw new Error('Failed to fetch users')
+          throw new Error("Failed to fetch users");
         }),
-  })
+  });
 
 export const userQueryOptions = (id: string) =>
   queryOptions({
-    queryKey: ['users', id],
+    queryKey: GET_SINGLE_USER_KEY(id),
     queryFn: () =>
       axios
-        .get<User>(DEPLOY_URL + '/api/users/' + id)
+        .get<User>(DEPLOY_URL + "/api/users/" + id)
         .then((r) => r.data)
         .catch(() => {
-          throw new Error('Failed to fetch user')
+          throw new Error("Failed to fetch user");
         }),
-  })
+  });
