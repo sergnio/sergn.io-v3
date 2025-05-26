8601dfb9c1605d7736589997eb506ff66d8a0170
sergnio <sergnioa@gmail.com>
2025-05-26 20:13:51 +0200

    always logout

--- src/routes/logout.route.tsx
+++ src/routes/logout.route.tsx
@@ -1,15 +1,20 @@
 import { createFileRoute, redirect } from "@tanstack/react-router";
-import { loggedInUserQueryOptions, logoutUser } from "~/utils/users";
+import { logoutUser } from "~/utils/users";
+import { GET_AUTHENTICATED_USERS_KEY } from "~/constants/query-keys";
 
 export const Route = createFileRoute("/logout")({
   preload: false,
   loader: async ({ context }) => {
-    console.log("Logging out user...");
-    await logoutUser();
-    await context.queryClient.invalidateQueries({
-      queryKey: loggedInUserQueryOptions().queryKey,
-    });
-    redirect({
+    // redirect to the home page if the user is not logged in
+    const user = context.queryClient.getQueryData(GET_AUTHENTICATED_USERS_KEY);
+    if (user) {
+      await logoutUser();
+      await context.queryClient.invalidateQueries({
+        queryKey: GET_AUTHENTICATED_USERS_KEY,
+      });
+    }
+
+    throw redirect({
       href: "/",
     });
   },
