efafdb7f700640faec2deb45b155aee2fc05ebf0
sergnio <sergnioa@gmail.com>
2025-05-26 20:17:31 +0200

    update seo

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -30,9 +30,8 @@ export const Route = createRootRouteWithContext<{
         content: "width=device-width, initial-scale=1",
       },
       ...seo({
-        title:
-          "TanStack Start | Type-Safe, Client-First, Full-Stack React Framework",
-        description: `TanStack Start is a type-safe, client-first, full-stack React framework. `,
+        title: "sergn.io",
+        description: `Behold this incredible website.`,
       }),
     ],
     links: [
