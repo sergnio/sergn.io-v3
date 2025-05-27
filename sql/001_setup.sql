dd1dcc36091671c6aadc7a404b72ec12717c7c4d
sergnio <sergnioa@gmail.com>
2025-05-27 11:34:29 +0200

    fix relationship with brew type

--- sql/001_setup.sql
+++ sql/001_setup.sql
@@ -1,19 +1,14 @@
--- ENUM for brew method
-create type brew_method_name as enum ('Moka Pot', 'Filter', 'v60', 'Espresso');
-
 -- ENUM for grinder type
 create type grinder_type as enum ('Manual', 'Niche Zero');
 
 -- Bag size table
 create table bag_size (
-  id uuid primary key default gen_random_uuid(),
-  oz integer,
-  g integer,
-  constraint check_only_one_size check (
-      (oz is not null and g is null) or
-      (oz is null and g is not null)
-      )
-);
+    id uuid primary key default gen_random_uuid(),
+    oz integer,
+    g integer,
+    constraint check_only_one_size check (
+        (oz is not null and g is null) or
+        (oz is null and g is not null)));
 
 -- Coffee table
 create table coffee (
@@ -23,27 +18,29 @@ create table coffee (
     price numeric,
     bag_size_id uuid references bag_size(id),
     image text
-);
+    );
 
 -- Grinder table
 create table grinder (
-     id uuid primary key default gen_random_uuid(),
-     type grinder_type not null,
-     number integer,
-     rotations integer,
-     setting integer,
-    -- constraint: only certain fields based on grinder type
-     constraint grinder_field_check check (
-         (type = 'Manual' and number is not null and rotations is not null and setting is null) or
-         (type = 'Niche Zero' and setting is not null and number is null and rotations is null)
-         )
-);
+    id uuid primary key default gen_random_uuid(),
+    type grinder_type not null,
+    number integer,
+    rotations integer,
+    setting integer,
+    constraint grinder_field_check check (
+(type = 'Manual' and number is not null and rotations is not null and setting is null) or
+(type = 'Niche Zero' and setting is not null and number is null and rotations is null)));
 
--- Brew methods per coffee
+-- Brew method table (deduplicated)
 create table brew_method (
-     id uuid primary key default gen_random_uuid(),
-     coffee_id uuid references coffee(id),
-     name brew_method_name not null,
-     grinder_id uuid references grinder(id)
-);
+    id uuid primary key default gen_random_uuid(),
+    name text not null unique
+    );
 
+-- Bridge table for coffee <> brew_method (many-to-many)
+create table coffee_brew_method (
+    id uuid primary key default gen_random_uuid(),
+    coffee_id uuid references coffee(id) on delete cascade,
+    brew_method_id uuid references brew_method(id),
+    grinder_id uuid references grinder(id)
+    );
