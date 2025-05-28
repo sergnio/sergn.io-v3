a0825936806ff48b6cb315645e9329ba9960036f
sergnio <sergnioa@gmail.com>
2025-05-28 11:01:10 +0200

    add storybook

--- src/components/atomic/Dropdown.tsx
+++ src/components/atomic/Dropdown.tsx
@@ -27,9 +27,9 @@ export const Dropdown = ({ label, options }: DropdownProps) => {
   let { contains } = useFilter({ sensitivity: "base" });
 
   return (
-    <Select name={camelize(label)}>
+    <Select name={camelize(label)} className="relative inline-block text-left">
       <Label>{label}</Label>
-      <Button>
+      <Button className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-xs ring-1 ring-gray-300 ring-inset hover:bg-gray-50">
         <SelectValue />
         <span aria-hidden="true">▼</span>
       </Button>
